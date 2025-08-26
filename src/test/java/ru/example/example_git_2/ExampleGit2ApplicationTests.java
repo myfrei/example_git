e4052f67ed6ae0ff4156e88e6608a9be023645cc
myfrei <myfrei97@gmail.com>
2025-08-26 20:35:00 +0400

    init

---
+++ src/test/java/ru/example/example_git_2/ExampleGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.example_git_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
